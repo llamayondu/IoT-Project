7dd2733d0c1940ddf66c5c8acc07d55e9c0e98fc
Shreeprabhas <shreeprabhas@gmail.com>
2024-04-24 22:32:42 +0530

    added /on route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash
 from graph2image import fetch_plots
+import execute
 
 app = Flask(__name__)
 channel_number = 2470056
@@ -9,6 +10,13 @@ secret_key = '6WA7QY0CC9HDX9UO'
 def home():
     fetch_plots(channel_number,secret_key,'static/images/')
     return render_template('index.html')
+
+@app.route('/on', methods=["POST"])
+def switch_on_ac():
+    print(f"{execute.talkback_id=}, {execute.key=}\n")
+    execute.execute(execute.talkback_id, execute.key, 'turn_on')
+    return
+    # return redirect(url_for("/"));
 
 if __name__ == '__main__':
     app.run(debug=True)
