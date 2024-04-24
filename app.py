f91d973e034eecfb3b358eb6e31cfdc5ccaa9e6a
unknown <vishalarao2004@gmail.com>
2024-04-24 22:11:44 +0530

    OwO daddy-chan bae is talking to us again (esp32)

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request, redirect, flash
 from graph2image import fetch_plots
 
 app = Flask(__name__)
+channel_number = 2470056
+secret_key = '6WA7QY0CC9HDX9UO'
 
 @app.route('/')
 def home():
-    fetch_plots()
+    fetch_plots(channel_number,secret_key,'static/images/')
     return render_template('index.html')
 
 if __name__ == '__main__':
---
+++ execute.py
@@ -0,0 +1,19 @@
+import requests
+import thingspeak
+
+key='RJUI9PUKJTWMP7ZB'
+talkback_id='52131'
+turn_on='41161931'
+turn_off='41161933'
+
+def execute(talkback_id,api_key,command_string):
+    command_id = requests.post('https://api.thingspeak.com/talkbacks/52131/commands',params={'api_key':'RJUI9PUKJTWMP7ZB','command_string':command_string})
+    form = requests.post(f'https://api.thingspeak.com/talkbacks/{talkback_id}/commands/execute.json',params={'api_key':api_key})
+    print(form.text)
+    # details of executed command
+
+#example:
+#execute(talkback_id,key,'turn_on')
+
+#PRABUSSY HELLO MAKE THIS FUNCTION EXECUTE WHEN USER CLICKS A BUTTON
+#IF USER CLICKS AC ON THEN MAKE THE COMMAND STRRING "TURN_ON" AND IF HE CLICKS AC OFF MAKE IT "TURN OFF"
