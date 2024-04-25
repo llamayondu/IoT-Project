f29c20aa983b7f5872b9ec33e25ac6a65a4c5424
unknown <vishalarao2004@gmail.com>
2024-04-25 13:45:10 +0530

    AC on and off UwU

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, redirect, url_for
-from graph2image import fetch_plots
 import execute
 
 app = Flask(__name__)
@@ -8,15 +7,21 @@ secret_key = '6WA7QY0CC9HDX9UO'
 
 @app.route('/')
 def home():
-    fetch_plots(channel_number,secret_key,'static/images/')
     return render_template('index.html')
 
 @app.route('/on', methods=["POST"])
 def switch_on_ac():
+    execute.delete_all_commands(execute.talkback_id,execute.key)
     print(f"{execute.talkback_id=}, {execute.key=}\n")
     execute.execute(execute.talkback_id, execute.key, 'TURN_ON') #ADDS TURN ON COMMAND TO THE QUEUE
     return redirect(url_for("home"))
     # return redirect(url_for("/"));
+@app.route('/off', methods=["POST"])
+def switch_off_ac():
+    execute.delete_all_commands(execute.talkback_id,execute.key)
+    print(f"{execute.talkback_id=}, {execute.key=}\n")
+    execute.execute(execute.talkback_id, execute.key, 'TURN_OFF') #ADDS TURN ON COMMAND TO THE QUEUE
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True)
